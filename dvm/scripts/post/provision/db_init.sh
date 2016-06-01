3cfc0d55d6f29e833ffc7caaea39997d5e344b67
Joe Bartlett <joe.bartlett@vml.com>
2016-06-01 02:13:00 -0500

    Committing today's dvm updates

--- dvm/scripts/post/provision/db_init.sh
+++ dvm/scripts/post/provision/db_init.sh
@@ -10,22 +10,23 @@ timestamp() {
 DBBK=/var/db_backups
 PROJECT_NAME=project_name
 FILE=(*.mysql.gz)
+WRITE_FILE=/home/vagrant/.db_init
 
-if [ ! -e ~/.db_init ]; then
+if [ ! -e $WRITE_FILE ]; then
   cd $DBBK
   if [ -e $DBBK/$FILE ]; then
     drush @$PROJECT_NAME.local sql-drop -y
     drush @$PROJECT_NAME.local sql-cli < $DBBK/$FILE
     drush @$PROJECT_NAME.local fra -y
     drush @$PROJECT_NAME.local updb -y
-    touch ~/.db_init
-    echo "$(timestamp): SQL restored from backup." >> ~/.db_init
+    touch $WRITE_FILE
+    echo "$(timestamp): SQL restored from backup." >> $WRITE_FILE
     cd
   else
-    touch ~/.db_init
-    echo "$(timestamp): No SQL backups were detected." >> ~/.db_init
+    touch $WRITE_FILE
+    echo "$(timestamp): No SQL backups were detected." >> $WRITE_FILE
     cd
   fi
 else
-  echo "$(timestamp): DB Init has already been run." >> ~/.db_init
+  echo "$(timestamp): DB Init has already been run." >> $WRITE_FILE
 fi
--- dvm/scripts/post/provision/solr_setup.sh
+++ dvm/scripts/post/provision/solr_setup.sh
@@ -8,22 +8,23 @@ timestamp() {
 # Set up Drupal SOLR
 PROJECT_URL=projecturl.com
 PROJECT_NAME=project_name
+WRITE_FILE=/home/vagrant/.solr_setup
 
 SOLR=/var/solr/$PROJECT_NAME
 MODULE_CONF=/var/www/$PROJECT_URL/htdocs/sites/all/modules/contrib/search_api_solr/solr-conf
 DVMCONF=/var/solr_files
 
-if [ ! -e ~/.solr_setup ]; then
+if [ ! -e $WRITE_FILE ]; then
   if [ -e $DVMCONF/conf/schema.xml ]; then
     sudo ln -s /var/solr_files/ $SOLR
     sudo chown -R solr:solr $SOLR
     sudo service solr restart
-    touch ~/.solr_setup
-    echo "$(timestamp): SOLR Core $PROJECT_NAME has been set up." >> ~/.solr_setup
+    touch $WRITE_FILE
+    echo "$(timestamp): SOLR Core $PROJECT_NAME has been set up." >> $WRITE_FILE
   else
-    touch ~/.solr_setup
-    echo "$(timestamp): No cores were detected in the dvm/solr/conf folder." >> ~/.solr_setup
+    touch $WRITE_FILE
+    echo "$(timestamp): No cores were detected in the dvm/solr/conf folder." >> $WRITE_FILE
   fi
 else
-  echo "$(timestamp): SOLR setup has already been run." >> ~/.solr_setup
+  echo "$(timestamp): SOLR setup has already been run." >> $WRITE_FILE
 fi
